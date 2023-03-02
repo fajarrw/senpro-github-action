c97fbf623a2e47f062f7d99ada7289b59aa6d6ed
Fajar Rizky Widodo <79750744+fajarrw@users.noreply.github.com>
2023-03-02 09:55:30 +0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
 
         <div>
           <h5 className={inter.className}>
-            [Nama] | [NIU]
+            [Fajar Rizky Widodo] | [463599]
           </h5>
         </div>
       </main>
